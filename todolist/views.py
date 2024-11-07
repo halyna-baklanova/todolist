7ba0e1db050f72de411d6756554d979fae0b0e60
halyna-baklanova <novabalka@gmail.com>
2024-11-07 12:04:40 +0200

    prepare index page

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from todolist.models import Task
+
+
+def index(request):
+    task_list = Task.objects.all()
+    return render(request, "todo/index.html", {"task_list": task_list})
